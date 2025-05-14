a44af8b6f7efb6a71ee026e7ada24f2e185fc8b0
Cassidy Helms <cassidy.helms@gmail.com>
2025-05-14 15:29:34 -0400

    reads all lines with /search

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "nokogiri", "~> 1.18"
+
+gem "rspec", "~> 3.13"
+
+
---
+++ spec/html_reader/html_reader_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../lib/html_reader/html_reader'
+
+RSpec.describe HtmlReader do
+  context "testing basic assertions" do
+    it "passes when true" do
+      expect(true).to eq true
+    end
+  end
+end
