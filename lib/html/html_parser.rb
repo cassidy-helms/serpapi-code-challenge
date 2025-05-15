6047bd19451030871085b0d544d4ad53996312be
Cassidy Helms <cassidy.helms@gmail.com>
2025-05-15 16:43:37 -0400

    update image to thumbnail if exists

--- lib/html/html_parser.rb
+++ lib/html/html_parser.rb
@@ -5,19 +5,19 @@ class HtmlParser
     ARTWORKS = 'Artworks'
   end
 
-  def self.parse(line)
+  def self.parse(html)
     results = SearchResults.new
-    parse_type = determineParseType(line)
+    parse_type = determineParseType(html)
 
     if(parse_type == ParseTypes::ARTWORKS)
-      results.artworks.concat(parseArtworks(line))
+      results.artworks.concat(parseArtworks(html))
     end
 
     results
   end
 
-  def self.determineParseType(line)
-    Nokogiri::HTML.parse(line).css('span').each do |span|
+  def self.determineParseType(html)
+    Nokogiri::HTML.parse(html).css('span').each do |span|
       if(span.text.strip == Artwork.heading)
         return parse_type = ParseTypes::ARTWORKS
       end
@@ -25,9 +25,9 @@ class HtmlParser
   end
   :private
 
-  def self.parseArtworks(line)
+  def self.parseArtworks(html)
       artworks = []
-      Nokogiri::HTML.parse(line).css('div div a').each {|a_tag| 
+      Nokogiri::HTML.parse(html).css('div div a').each {|a_tag| 
         if(a_tag['href'] =~ /\/search/)
           link = a_tag['href']
 
@@ -39,13 +39,32 @@ class HtmlParser
           image_src = image_tag['src'] if image_tag
           image_data_src = image_tag['data-src'] if image_tag
           image = image_data_src || image_src
+          image_id = image_tag['id'] if image_tag
 
           if !extensions.empty? && [link, name, extensions[0], image].all? { |v| v.to_s.strip != "" }            
-            artworks << Artwork.new(name, extensions, "https://www.google.com#{link}", image)
+            artworks << Artwork.new(name, extensions, "https://www.google.com#{link}", image, image_id)
           end
         end
       }
+
+      artworks.each {|artwork|
+        if(artwork.image_id)
+          doc = Nokogiri::HTML.parse(html).xpath("//script[contains(text(), '#{artwork.image_id}')]")
+          image_src = extract_source_variable_from_javascript_string(doc.text)
+          
+          artwork.image = image_src if image_src
+        end
+      }
+
       return artworks
+  end
+  :private
+
+  def self.extract_source_variable_from_javascript_string(js_string)
+    if js_string =~ /var\s+s\s*=\s*'([^']+)'/
+      return $1
+    end
+    nil
   end
   :private
 end
--- lib/html/search_result_types/Artwork.rb
+++ lib/html/search_result_types/Artwork.rb
@@ -1,12 +1,14 @@
 class Artwork
-  attr_reader :name, :extensions, :link, :image
+  attr_reader :name, :extensions, :link, :image, :image_id
+  attr_accessor :image
   @@heading = 'Artworks'
 
-  def initialize(name, extensions, link, image)
+  def initialize(name, extensions, link, image, image_id)
     @name = name
     @extensions = extensions
     @link = link
     @image = image
+    @image_id = image_id
   end
 
   def self.heading
